38c586161d92f8735681c4e27bb3cc4bd4c0e907
unknown <emmanuelileberi@gmail.com>
2023-06-23 11:23:39 +0200

    first commit

---
+++ greet.test.js
@@ -0,0 +1,18 @@
+describe('greetfunction' , function(){
+    it('should greet someone' , function(){
+        
+        assert.equal(greet('Bob'), 'Hello, Bob');
+        
+    });
+    it('should greet someone' , function(){
+        
+        assert.equal(greet('Sam'), 'Hello, Sam');
+        
+    });
+});
+
+
+
+
+
+
